bea05904957f348db855c7586f9ac98dad59ff8f
Jonatha Lira <>
2022-12-29 20:02:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import math 
+import random
+
+#desafio 12
+#preco = float(input('Qual é o preço do produto ? R$'))
+#desconto = preco*0.05
+#novo_preco = preco - desconto  
+#print('O produto que custava R${}, na promoção com desconto de 5% vai custar R${:.2f}'.format(preco, novo_preco))
+
+#desafio 13
+#salario = float(input('Qual é o salário do funcionário ? R$'))
+#novo_salario = (salario*0.15) + salario
+#print('Um funcionário que ganhava R${}, com 15% de aumento, passa #a receber R${:.2f}'.format(salario, novo_salario))
+
+#desafio 14
+#temp = float(input('Informe a temperatura em °C: '))
+#temp_em_farenheit = (temp*1.8)+32
+#print('A temperatura de {}°C corresponde a {}°F'.format(temp, #temp_em_farenheit))
+
+#desafio 15
+#dias_alugados = int(input('Quantos dias alugados ? '))
+#km_rodados = float(input("Quantos km's rodados ? "))
+#aluguel_dia = dias_alugados*60
+#km_pagar = km_rodados*0.15
+#total = aluguel_dia + km_pagar
+#print('O total a pagar por {} dias alugados e {} por km rodados é {:.2f}'.format(dias_alugados, km_rodados, total))
+
+#desafio 16
+#n = float(input('Digite um valor: '))
+#inteiro = math.trunc(n)
+#print('O valor digitado foi {} e o seu valor inteiro é {}'.format(n, inteiro))
+
+#desafio 17
+#cot = float(input('Comprimento do cateto oposto: '))
+#cad = float(input('Comprimento do cateto adjacente: '))
+#hip = math.hypot(cad, cot)
+#print('A hipotenusa vai valer {:.2f}'.format(hip))
+
+#desafio 18
+#ang = int(input('Digite o ângulo que você deseja: '))
+#sen = math.sin(math.radians(ang))
+#cos = math.cos(math.radians(ang))
+#tang = math.tan(math.radians(ang))
+#print('O ângulo de {} tem seno de {:.2f}. \nO ângulo de {} tem cosseno de {:.2f}. \nO ângulo de {} tem tangente de {:.2f}.'.format(ang, sen, ang, cos, ang, tang))
+
+#desafio 19
+#aluno1 = str(input('Nome do aluno(a): '))
+#aluno2 = str(input('Nome do aluno(a): '))
+#aluno3 = str(input('Nome do aluno(a): '))
+#aluno4 = str(input('Nome do aluno(a): '))
+#escolhido = random.choice([aluno1, aluno2, aluno3, aluno4])
+#print('O aluno(a) foi: {}'.format(escolhido))
+
+#desafio 20
+#aluno1 = str(input('Nome do aluno(a): '))
+#aluno2 = str(input('Nome do aluno(a): '))
+#aluno3 = str(input('Nome do aluno(a): '))
+#aluno4 = str(input('Nome do aluno(a): '))
+#escolhido = [aluno1, aluno2, aluno3, aluno4]
+#random.shuffle(escolhido)
+#print('O aluno(a) foi: {}'.format(escolhido))
